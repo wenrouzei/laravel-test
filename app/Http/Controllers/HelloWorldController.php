b7793508797060cd24be4a6880ede7c262fbbedb
root <root@localhost.localdomain>
2016-11-30 10:50:27 +0800

    compact 传值模板测试

--- app/Http/Controllers/HelloWorldController.php
+++ app/Http/Controllers/HelloWorldController.php
@@ -11,23 +11,23 @@ class HelloWorldController extends Controller
     //
     public function index(){
         // var_dump(config('auth.defaults'));
-        var_dump(getenv('APP_KEY'));//获取全局环境变量
-        echo "<br>";
-        echo "<br>";
-        var_dump($_ENV);//全局环境变量 .env文件定义
+        // var_dump(getenv('APP_KEY'));//获取全局环境变量
+        // echo "<br>";
+        // echo "<br>";
+        // var_dump($_ENV);//全局环境变量 .env文件定义
     	// echo "<pre>";
     	// $articles = \App\Article::find(2);
     	// var_dump($articles);
     	$articles = Article::where('id', '>', 2)->where('id', '<', 20)->orderBy('updated_at', 'desc')->get();
 
-    	foreach ($articles as $article) {
+    	// foreach ($articles as $article) {
 
-    	    echo $article->title."<br>";
+    	//     echo $article->title."<br>";
 
-    	}
+    	// }
 
     	// throw new \Exception("Error Processing Request", 1);
-    	
-    	return view('HelloWorld');
+    	$title = 'compact — 建立一个数组，包括变量名和它们的值 传值模板测试';
+    	return view('HelloWorld', compact('articles', 'title'));
     }
 }
--- resources/views/HelloWorld.blade.php
+++ resources/views/HelloWorld.blade.php
@@ -1,1 +1,6 @@
 hello world
+<h1>{{ $title }}</h1>
+@foreach($articles as $article)
+<p>This is the title: {{$article->title}}</p>
+@endforeach
+			
