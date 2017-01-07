8c3a7b3acf3fd3489fd5944c14f950af07ddd696
lee <wenrouzei@163.com>
2017-01-07 14:13:53 +0800

    add 添加migrate方式后期给数据库表添加字段测试：:w

---
+++ database/migrations/2017_01_07_140249_add_publishAt_column_to_article.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPublishAtColumnToArticle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->timestamp('pulish_at')->nullable();
+        });
+    }
+
+    /**：去
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->timestamp('publish_at')->nullable();
+        });
+    }
+}
